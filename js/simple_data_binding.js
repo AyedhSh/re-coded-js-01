131d5a2c99783f517d47e258603f5df3e80b2625
AyedhSh <alshmary_ad@yahoo.com>
2017-02-28 23:34:41 +0300

    add solution

--- js/simple_data_binding.js
+++ js/simple_data_binding.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
-  
+  printToDiv();
 });
+
+function printToDiv(){
+
+  $('#textArea').on('keyup', function(key) {
+
+    if(key.which == 8)
+      {$('#myDiv').text(function (_,txt) {
+        return txt.slice(0, -1);
+      });}
+    else if (key.which == 13)
+      { $('#myDiv').append('<br/>');}
+
+    else
+     { $('#myDiv').append(String.fromCharCode(key.which).toLowerCase());}
+
+ });
+
+}
